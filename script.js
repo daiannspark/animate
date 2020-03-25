c714daea5648441ddd258c51641d9713cd8889f0
Diana Markevich <daiannspark@users.noreply.github.com>
2020-03-25 21:02:23 +0200

    Upload

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $(".left").click(function(){
+        $(".image").animate({
+            left: '+=50px'
+        });
+    });
+
+    $(".right").click(function(){
+        $(".image").animate({
+            left: '+=50px'
+        });
+    });
+
+    $(".top").click(function(){
+        $(".image").animate({
+            bottom: '+=50px'
+        });
+    });
+
+    $(".bottom").click(function(){
+        $(".image").animate({
+            top: '+=50px'
+        });
+    });
+});
